17f5ef9afd624dec0479b4b24cd0b6ef36fb8d6b
JovenTung <joventung88@gmail.com>
2024-06-06 02:16:32 +0800

    Added in discord bot commands

---
+++ src/base/classes/CustomClient.ts
@@ -0,0 +1,18 @@
+import { Client } from "discord.js";
+import ICustomClient from "../interfaces/ICustomClient";
+import { IConfig } from "../interfaces/IConfig";
+
+export default class CustomClient extends Client implements ICustomClient {
+  config: IConfig;
+
+  constructor() {
+    super({ intents: [] });
+
+    this.config = require("${process.cwd()}dataconfig.json");
+  }
+  Init(): void {
+    this.login(this.config.token)
+      .then(() => console.log("Logged In"))
+      .catch((err) => console.error(err));
+  }
+}
